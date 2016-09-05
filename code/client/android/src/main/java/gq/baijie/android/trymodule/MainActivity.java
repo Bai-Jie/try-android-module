c19ecdaf1ed2d58e9df4c3edc8493ed10ae6b238
Bai Jie <baijie1991@gmail.com>
2016-09-05 14:14:38 +0800

    add test menu

--- code/client/android/src/main/java/gq/baijie/android/trymodule/MainActivity.java
+++ code/client/android/src/main/java/gq/baijie/android/trymodule/MainActivity.java
@@ -2,6 +2,9 @@ package gq.baijie.android.trymodule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+
+    menu.add("Hello").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+      @Override
+      public boolean onMenuItemClick(MenuItem menuItem) {
+        Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+        return true;
+      }
+    });
+
+    return true;
   }
 }
