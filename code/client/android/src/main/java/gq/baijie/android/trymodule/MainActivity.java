63a8da0d25d2deb62137c01ee8611408ced38c6d
Bai Jie <baijie1991@gmail.com>
2016-09-05 15:28:11 +0800

    add navigation drawer

--- code/client/android/src/main/java/gq/baijie/android/trymodule/MainActivity.java
+++ code/client/android/src/main/java/gq/baijie/android/trymodule/MainActivity.java
@@ -1,30 +1,78 @@
 package gq.baijie.android.trymodule;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-import gq.baijie.android.trymodule.business.Greeting;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    // init toolbar
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    // init drawer
+    initDrawer(toolbar);
+  }
+
+  private void initDrawer(Toolbar toolbar) {
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    drawer.addDrawerListener(toggle);
+    toggle.syncState();
+
+    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
   }
 
   @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    super.onCreateOptionsMenu(menu);
+  public void onBackPressed() {
+    if (!closeDrawer()) {
+      super.onBackPressed();
+    }
+  }
 
-    menu.add("Hello").setOnMenuItemClickListener(menuItem -> {
-      final String text = Greeting.getInstance().say("World");
-      Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
+  private boolean closeDrawer() {
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    if (drawer.isDrawerOpen(GravityCompat.START)) {
+      drawer.closeDrawer(GravityCompat.START);
       return true;
-    });
+    } else {
+      return false;
+    }
+  }
 
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+
+    if (id == R.id.nav_camera) {
+      // Handle the camera action
+    } else if (id == R.id.nav_gallery) {
+
+    } else if (id == R.id.nav_slideshow) {
+
+    } else if (id == R.id.nav_manage) {
+
+    } else if (id == R.id.nav_share) {
+
+    } else if (id == R.id.nav_send) {
+
+    }
+
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    drawer.closeDrawer(GravityCompat.START);
     return true;
   }
 }
